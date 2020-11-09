026a47253570b9353d02c34fff92ba1dc3fa0f8b
Dylan King <17197813@studentmail.ul.ie>
2020-11-09 15:22:32 +0000

    Added tests for data processor struct creation. Also learned about threads on the way.

--- test/agents_test/data_processor_agent_test.go
+++ test/agents_test/data_processor_agent_test.go
@@ -1,10 +1,49 @@
 package test
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"../../src/agents"
 )
 
-func data_processor_placeholder_test(t *testing.T) {
-	assert.Equal(t, 0, 0)
+func Test_CheckoutUsageData_Creation(t *testing.T) {
+	actualValue := agents.CheckoutUsageData{
+		CheckoutNum:             0,
+		TimeSpent:               0.0,
+		TotalCustomersProcessed: 0,
+	}
+
+	assertCheckoutUsageData(t, &actualValue, 0, 0.0, 0)
+}
+
+func Test_DataProcessor_Creation(t *testing.T) {
+	actualValue := agents.DataProcessor{
+		CheckoutUsage:      make(chan *agents.CheckoutUsageData),
+		AvgCheckoutUseTime: 0.0,
+	}
+
+	go func() {
+		actualValue.CheckoutUsage <- &agents.CheckoutUsageData{
+			CheckoutNum:             0,
+			TimeSpent:               0.0,
+			TotalCustomersProcessed: 0,
+		}
+	}()
+
+	actualCheckoutUsage := <-actualValue.CheckoutUsage
+
+	assertCheckoutUsageData(t, actualCheckoutUsage, 0, 0.0, 0)
+
+	assert.Equal(t, actualValue.AvgCheckoutUseTime, 0.0)
+}
+
+func assertCheckoutUsageData(
+	t *testing.T, actual *agents.CheckoutUsageData,
+	num int, spent float64, processed int,
+) {
+	assert.Equal(t, actual.CheckoutNum, num)
+	assert.Equal(t, actual.TimeSpent, spent)
+	assert.Equal(t, actual.TotalCustomersProcessed, processed)
 }
