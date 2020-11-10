7815a1f4f8a741c9a70da56f4db2a9e0aaf092c2
Louise Madden <17198232@studentmail.ul.ie>
2020-11-10 17:08:39 +0000

    Added method/function signatures and lots of comments

---
+++ src/agents/checkout_agent.go
@@ -0,0 +1,69 @@
+package agents
+
+// Checkout struct with potential item limit, 5/10 and queue limit, usually 6, possibly 4 for COVID
+type Checkout struct {
+	ItemLimit  int
+	QueueLimit int
+	//Queue [QueueLimit]Customer
+}
+
+// TODO: create an array of customers with a buffer of 6/ variable that is passed in? COVID
+// TODO: be able to scan items, time/speed, multiplied by the item size multiplier
+// TODO: check the time it took for each customer
+
+// NewCheckout creates a checkout
+func NewCheckout(itemLim int, queueLim int) *Checkout {
+	//var QueueB [QueueLimit]Customer 
+	checkout := Checkout{
+		ItemLimit : itemLim
+		QueueLimit : queueLim
+		//Queue : QueueB
+	}
+	return &checkout
+}
+// JoinCheckout takes in customer 
+// TODO: add to queue array, loop and find first nil??? assign to that index
+func (checkout *Checkout) JoinCheckout(customer *Customer) {
+
+}
+
+// ServeCustomer serves a single customer
+// TODO: Take in first customer
+// TODO: Loop through the item array
+// TODO: Run ScanItem method
+// TODO: Shift all customers up 1 index in array/space in queue
+// TODO: Pass info to Dylan's data processor
+func (checkout *Checkout) ServeCustomer() {
+	// BEGINNING TIME
+	current := Queue[0] //current is customer
+	for i, v := range current.Trolley {
+		ScanItem(v)
+	}
+	// END TIME
+	// SUBTRACT BEGINNING FROM END
+
+	// If we want to add sleep for bagging and paying, add here
+	MoveQueue()
+	LeaveStore(current)
+}
+
+// ScanItem scans the current item for the current customer
+// TODO: Multiply a constant by weight
+func ScanItem(item *Product) {
+	
+}
+
+// MoveQueue removes the first customer(index 0) from the queue
+// TODO: Remove first customer
+// TODO: Move other customers up an index
+func (checkout *Checkout) MoveQueue() {
+
+}
+
+// LeaveStore is the customer leaving the store, and calling data processor
+// TODO: Have an if statement to see if customer went through checkout or left angry
+// TODO: If through checkout send what is necessary for Dylan
+// TODO: If angry send that they were angry
+func LeaveStore(customer *Customer) {
+
+}
