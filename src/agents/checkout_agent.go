d08943e4be3d3686b8a2d6f157bf5991d28e8037
LouiseMadden <37147382+LouiseMadden@users.noreply.github.com>
2020-11-26 10:09:10 +0000

    Checkout initialisation (#16)
    * Updated Queue to channel, made customer leaving queue atomic operation, removed unnecessary methods
    Co-authored-by: Dylan King <17197813@studentmail.ul.ie>

--- src/agents/checkout_agent.go
+++ src/agents/checkout_agent.go
@@ -1,1 +1,73 @@
+package agents
 
+import (
+	"sync"
+	"sync/atomic"
+)
+
+// Checkout struct with potential item limit, 5/10 and queue limit, usually 6, possibly 4 for COVID
+type Checkout struct {
+	ItemLimit       int
+	QueueLimit      int
+	Queue           chan *Customer
+	CurrentQueueLen int64
+	QueueLock       sync.Mutex
+}
+
+// NewCheckout creates a checkout
+func NewCheckout(itemLim int, queueLim int) *Checkout {
+	var theQueue = make(chan *Customer)
+	checkout := Checkout{
+		ItemLimit:  itemLim,
+		QueueLimit: queueLim,
+		Queue:      theQueue,
+	}
+	return &checkout
+}
+
+// JoinCheckout takes in customer
+// TODO: move this to Brian and update
+// func (checkout *Checkout) JoinCheckout(customer *Customer) {
+// 	checkout.QueueLock.Lock()
+
+// 	for i = 0; i < checkout.Queue.length; i++ {
+// 		if checkout.Queue[i] == nil {
+// 			checkout[i] = customer
+// 			CurrentQueueLen++
+// 			break
+// 		}
+// 	}
+
+// 	checkout.QueueLock.Unlock()
+
+// }
+
+// ServeCustomer serves a single customer
+// TODO: Take in first customer
+// TODO: Loop through the item array
+// TODO: Run ScanItem method
+// TODO: Shift all customers up 1 index in array/space in queue
+// TODO: Pass info to Dylan's data processor
+func (checkout *Checkout) ServeCustomer() {
+
+	for {
+		// BEGINNING TIME
+		customer, ok := <-checkout.Queue
+		if ok {
+			atomic.AddInt64(&checkout.CurrentQueueLen, -1)
+			for i, v := range customer.Trolley {
+				ScanItem(v)
+			}
+			// If we want to add sleep for bagging and paying, add here
+			customer.checkedOut = true
+		}
+		// END TIME
+		// SUBTRACT BEGINNING FROM END
+	}
+}
+
+// ScanItem scans the current item for the current customer
+// TODO: Multiply a constant by weight
+func ScanItem(item *Product) {
+
+}
